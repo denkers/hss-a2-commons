63eb53aa4b597f387336803afbcd021403b64d3b
denkers <kyle.business@inbox.com>
2016-10-12 17:04:56 +1300

    Updated RequestPaths with constant name changes

--- src/com/kyleruss/hssa2/commons/CryptoUtils.java
+++ src/com/kyleruss/hssa2/commons/CryptoUtils.java
@@ -6,7 +6,17 @@
 
 package com.kyleruss.hssa2.commons;
 
+import java.io.UnsupportedEncodingException;
+import java.security.Key;
+import java.security.KeyFactory;
+import java.security.NoSuchAlgorithmException;
+import java.security.PublicKey;
 import java.security.SecureRandom;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.KeySpec;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.security.spec.X509EncodedKeySpec;
+import java.util.Base64;
 
 public class CryptoUtils 
 {
@@ -35,6 +45,16 @@ public class CryptoUtils
         
         return builder.toString();
     }
+    
+    public static Key stringToAsymKey(String keyValue, boolean decode, boolean publicKey) 
+    throws UnsupportedEncodingException, NoSuchAlgorithmException, InvalidKeySpecException
+    {
+        byte[] keyBytes             =   decode? Base64.getDecoder().decode(keyValue.getBytes("UTF-8")) : keyValue.getBytes("UTF-8");
+        KeySpec keySpec             =   publicKey? new X509EncodedKeySpec(keyBytes) : new PKCS8EncodedKeySpec(keyBytes);   
+        KeyFactory keyFactory       =   KeyFactory.getInstance("RSA");
+        return publicKey? keyFactory.generatePublic(keySpec) : keyFactory.generatePrivate(keySpec);
+    }
+    
     
     public static byte[] generateRandomBytes(int length)
     {
--- src/com/kyleruss/hssa2/commons/RequestPaths.java
+++ src/com/kyleruss/hssa2/commons/RequestPaths.java
@@ -10,19 +10,19 @@ public class RequestPaths
 {
     private RequestPaths() {}
     
-    public static final String SERV_KEY_REQ     =   "/key/server/public/fetch";
+    public static final String USER_PUBLIC_GET_REQ      =   "/key/user/public/fetch";
     
-    public static final String SERV_CONNECT_REQ =   "/user/connect";
+    public static final String SERV_CONNECT_REQ         =   "/user/connect";
     
-    public static final String SERV_DISCON_REQ  =   "/user/disconnect";
+    public static final String SERV_DISCON_REQ          =   "/user/disconnect";
     
-    public static final String PASS_REQ         =   "/user/password/send";
+    public static final String PASS_REQ                 =   "/user/password/send";
     
-    public static final String PUBLIC_SEND_REQ  =   "/key/user/add";
+    public static final String USER_PUBLIC_SEND_REQ     =   "/key/user/add";
     
-    public static final String USER_LIST_REQ    =   "/user/online/list";
+    public static final String USER_LIST_REQ            =   "/user/online/list";
     
-    public static final String PUBLIC_GET_REQ   =   "/key/server/public/get";
+    public static final String SERV_PUBLIC_GET_REQ      =   "/key/server/public/get";
     
-    public static final String PROFILE_UP_REQ   =   "/user/profile/upload";
+    public static final String PROFILE_UP_REQ           =   "/user/profile/upload";
 }
